3401d07ada3c5ed6c96b064197e4c38ae13a7388
thomasgoelles <thomas.goelles@gmail.com>
2022-08-04 10:39:25 +0200

    test

--- v0.1/js/ros.js
+++ v0.1/js/ros.js
@@ -8,5 +8,8 @@ function writeContent(fileUrl, file, title, authors) {
     //$('.preview').append($("<audio/>").attr("controls", "").attr("src", fileUrl));
     //$('.preview').append("test");
     //$(".preview-note").text("test")
-    $('.preview').append("https://studio.foxglove.dev/?ds=ros1-remote-bagfile&ds.url=https%3A%2F%2Fgithub.com%2Fvirtual-vehicle%2Fpointcloudset%2Fraw%2Fmaster%2Ftests%2Ftestdata%2Ftest.bag&layoutId=d8890d0c-1e3a-4730-9d62-f1e40f51e8e1");
+    var div = document.getElementById('divID');
+    div.innerHTML += 'Extra stuff';
+    //$('.preview').append("https://studio.foxglove.dev/?ds=ros1-remote-bagfile&ds.url=https%3A%2F%2Fgithub.com%2Fvirtual-vehicle%2Fpointcloudset%2Fraw%2Fmaster%2Ftests%2Ftestdata%2Ftest.bag&layoutId=d8890d0c-1e3a-4730-9d62-f1e40f51e8e1");
 }
+
