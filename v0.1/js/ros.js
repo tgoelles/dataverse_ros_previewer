826b663497493024962cca41393a8a6380b31215
thomasgoelles <thomas.goelles@gmail.com>
2022-08-04 11:46:50 +0200

    test translate

--- v0.1/js/ros.js
+++ v0.1/js/ros.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     startPreview(true);
 });
 
+function translateBaseHtmlPage() {
+    var audioPreviewText = $.i18n("audioPreviewText");
+    $('.audioPreviewText').text(audioPreviewText);
+}
+
 function writeContent(fileUrl, file, title, authors) {
     addStandardPreviewHeader(file, title, authors);
     //$('.preview').append($("<audio/>").attr("controls", "").attr("src", fileUrl));
